fd50800eef1a568e9c282f5f8439e9593135456d
Akash Ranjan Swain <aakashswain001@gmail.com>
2018-05-19 10:48:31 +0530

    Bottombar added with animation in fullscreen image

--- app/src/main/java/xyz/sleepygamers/agallery/FullScreenImage/GalleryPreview.java
+++ app/src/main/java/xyz/sleepygamers/agallery/FullScreenImage/GalleryPreview.java
@@ -10,6 +10,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,14 +25,15 @@ import xyz.sleepygamers.agallery.utils.Function;
 import xyz.sleepygamers.agallery.R;
 
 
-public class GalleryPreview extends AppCompatActivity {
+public class GalleryPreview extends AppCompatActivity implements ViewPager.OnPageChangeListener {
 
     ArrayList<HashMap<String, String>> imageList = new ArrayList<HashMap<String, String>>();
     private FullScreenImageAdapter adapter;
     private ViewPager viewPager;
     Toolbar mToolbar;
     boolean mToolbarVisibility = true;
-    AppBarLayout mAppbar;
+    LinearLayout bottomBar;
+    ImageButton ib_edit, ib_crop, ib_delete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,35 +45,30 @@ public class GalleryPreview extends AppCompatActivity {
         setSupportActionBar(mToolbar);
 
         viewPager = (ViewPager) findViewById(R.id.pager);
-
         Intent i = getIntent();
         int position = i.getIntExtra("position", 0);
         imageList = (ArrayList<HashMap<String, String>>) i.getExtras().getSerializable("list");
-
         adapter = new FullScreenImageAdapter(GalleryPreview.this,
                 imageList);
-
         viewPager.setAdapter(adapter);
         setTitle();
         // displaying selected image first
         viewPager.setCurrentItem(position);
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        viewPager.addOnPageChangeListener(this);
+        setBottomBar();
+    }
+
+    private void setBottomBar() {
+        bottomBar = findViewById(R.id.bottomBar);
+        ib_edit = findViewById(R.id.ib_edit);
+        ib_crop = findViewById(R.id.ib_crop);
+        ib_delete = findViewById(R.id.ib_delete);
+        ib_edit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                setTitle();
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
+            public void onClick(View v) {
+                editPic();
             }
         });
-
     }
 
     @Override
@@ -100,9 +102,14 @@ public class GalleryPreview extends AppCompatActivity {
     void setToolbarView() {
         if (mToolbarVisibility) {
             getSupportActionBar().hide();
+            //   bottomBar.setVisibility(View.GONE);
+            bottomBar.animate().translationY(bottomBar.getHeight()).setInterpolator(new DecelerateInterpolator(1));
             //    mToolbar.animate().translationY(-mToolbar.getHeight()).setInterpolator(new AccelerateInterpolator(2));
         } else {
             getSupportActionBar().show();
+            //bottomBar.setVisibility(View.VISIBLE);
+            bottomBar.animate().translationY(0).setInterpolator(new AccelerateInterpolator(1));
+
             //     mToolbar.animate().translationY(0).setInterpolator(new DecelerateInterpolator(2));
         }
         mToolbarVisibility = !mToolbarVisibility;
@@ -112,10 +119,27 @@ public class GalleryPreview extends AppCompatActivity {
         String path = imageList.get(+viewPager.getCurrentItem()).get(Function.KEY_PATH);
         try {
             path = path.substring(path.lastIndexOf("/") + 1);
+            if (path.length() > 15) {
+                path = path.substring(0, 15).concat("...");
+            }
             getSupportActionBar().setTitle(path);
         } catch (Exception ex) {
             //        Toast.makeText(GalleryPreview.this, ex.getMessage(), Toast.LENGTH_SHORT).show();
         }
     }
 
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        setTitle();
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
 }
